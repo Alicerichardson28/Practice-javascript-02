b6458e9049f980a4f20e8cf5f570a2a4ddf0ef92
Parada <paradafaii28@gmail.com>
2021-05-01 15:44:52 -0400

    fuction calling other functions

--- script.js
+++ script.js
@@ -43,32 +43,48 @@
 
 
 // function declaration
-function calcAge1(birthYeah) {
-    return 2021 - birthYeah;
-}
-const age1 = calcAge1(1991);
+// function calcAge1(birthYeah) {
+//     return 2021 - birthYeah;
+// }
+// const age1 = calcAge1(1991);
 
 
 // function expression you have to store function in the variable // function without the name call anonymous function
-const calcAge2 = function (birthYeah) {
-    return 2021 - birthYeah;
-}
-const age2 = calcAge2(1991);
+// const calcAge2 = function (birthYeah) {
+//     return 2021 - birthYeah;
+// }
+// const age2 = calcAge2(1991);
 
 
-console.log(age1, age2);
+// console.log(age1, age2);
 
 // Arrow function
-const calcAge3 = birthYeah => 2021 - birthYeah;
-const age3 = calcAge3(1991);
-console.log(age3);
+// const calcAge3 = birthYeah => 2021 - birthYeah;
+// const age3 = calcAge3(1991);
+// console.log(age3);
 
-const yearsUntilRetirement = (birthYeah, firstName) => {
-    const age = 2021 - birthYeah;
-    const retirement = 65 - age;
-    // return retirement
-    return `${firstName} retires in ${retirement} years`;
+// const yearsUntilRetirement = (birthYeah, firstName) => {
+//     const age = 2021 - birthYeah;
+//     const retirement = 65 - age;
+//     // return retirement
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Alice'));
+// console.log(yearsUntilRetirement(1989, 'Taylor'));
+
+
+// function calling other functions
+function cutFruitPieces(fruit) {
+    return fruit * 4;
 }
 
-console.log(yearsUntilRetirement(1991, 'Alice'));
-console.log(yearsUntilRetirement(1989, 'Taylor'));
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
