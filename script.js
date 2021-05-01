09e8e3927cd0466f845d2a94cbce823a3f4b5581
Parada <paradafaii28@gmail.com>
2021-05-01 14:28:18 -0400

    funtion declaration

--- script.js
+++ script.js
@@ -11,32 +11,38 @@
 // const interface = 'Audio';
 // const private = 534;
 
-function logger() {
-    console.log('My name is Alice');
-}
+// function logger() {
+//     console.log('My name is Alice');
+// }
 
 // invoking / calling / running function then whatever in the function will get execute 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
 /*parameter, variable will specific to this function only, will get define when we call the function 
 // variable will present input data in this function
 */
 
-function fruitProcessor(apples, oranges) {
-    // console.log(apples, oranges)
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//     // console.log(apples, oranges)
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
 
 // actual variable value call argument
 // fruitProcessor(5, 0);
 
 // capture/ store function in to variable
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-console.log(fruitProcessor(5, 0));
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+function calcAge1(birthYeah) {
+    return 2021 - birthYeah;
+}
+
+console.log(calcAge1(1991))
