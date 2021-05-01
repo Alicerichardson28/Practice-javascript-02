9d3e7ad0578f975595137ae5a15201faff85c9c0
Parada <paradafaii28@gmail.com>
2021-04-30 20:11:19 -0400

    learn use strict for helping debug

---
+++ script.js
@@ -0,0 +1,12 @@
+// for better debugger
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive')
+
+// use strict will help debugging when you use the reserved word
+// const interface = 'Audio';
+// const private = 534;
