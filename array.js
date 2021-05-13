070f2e80e21ea48479858e332ccf93abaa750411
Parada <paradafaii28@gmail.com>
2021-05-13 11:44:39 -0400

    code challenge with array

--- array.js
+++ array.js
@@ -63,9 +63,35 @@ console.log(friends)
 
 //remove first elements in the array
 friends.shift();
-console.log(friends)
+console.log(friends);
 
 
 // check index position, if the element not in the array will return -1
-console.log(friends.indexOf('Alice'))
-console.log(friends.indexOf('Bob'))
+console.log(friends.indexOf('Alice'));
+console.log(friends.indexOf('Bob'));
+
+// Es6 method includes return index of the element and also return true or false 
+// Also is strict equality 
+friends.push(23)
+console.log(friends.includes('Alice'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Taylor')) {
+    console.log('You have a friend called Taylor');
+}
+
+
+// Coding challenge #2
+
+const calcTip = (bill) => {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(bills, tips, totals)
+
+
+
