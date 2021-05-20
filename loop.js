70a33226db42b95c24e5867c3910aba0a0b4fc69
Parada <paradafaii28@gmail.com>
2021-05-20 09:58:31 -0400

    for loop structure and how it work

---
+++ loop.js
@@ -0,0 +1,12 @@
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+
+// for loop have counter // keeps running while condition is TRUE
+
+for(let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weights repetition ${rep}`);
+}
