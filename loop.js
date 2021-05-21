856161453f28e32e715d505cb9d6a98f4e356eeb
Parada <paradafaii28@gmail.com>
2021-05-21 10:32:41 -0400

    looping array

--- loop.js
+++ loop.js
@@ -10,3 +10,26 @@
 for(let rep = 1; rep <= 10; rep++){
     console.log(`Lifting weights repetition ${rep}`);
 }
+
+// looping array
+
+const alice= [
+    'Alice',
+    'Richardson',
+    2021 - 1997,
+    'Developer',
+    ['Peter', 'Steven', 'Michael'],
+    true
+];
+
+// i start from 0 because the array base of 0 is first element
+
+// console.log(aliceArray[0])
+// console.log(aliceArray[1])
+// console.log(aliceArray[2])
+// console.log(aliceArray[3])
+// console.log(aliceArray[4])
+
+for(let i = 0; i < alice.length; i++){
+    console.log(alice[i], typeof alice[i]);
+}
